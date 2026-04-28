925e60e5dd0d26b090c97d2178751c117aa061d6
RidhaFerchichi404 <ridha.ferchichi2222@hotmail.com>
2026-04-28 18:29:41 +0100

    fixed issues with the pipeline

--- ci/pipelines/microservicePipeline.groovy
+++ ci/pipelines/microservicePipeline.groovy
@@ -140,7 +140,7 @@ def runMicroservicePipeline(Map cfg) {
                             return
                         }
                         try {
-                            timeout(time: 15, unit: "MINUTES") {
+                            timeout(time: 15, unit: "SECONDS") {
                                 def qualityGate = waitForQualityGate abortPipeline: false
                                 if (qualityGate?.status != "OK") {
                                     echo "SonarQube Quality Gate status: ${qualityGate?.status}. Continuing without marking build unstable."
